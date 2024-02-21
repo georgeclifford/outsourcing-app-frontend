249ffb7bc31cbf9a6d655b325acb862912e58476
georgeclifford <grgclffrd@gmail.com>
2024-02-22 01:43:09 +0530

    completed employee details page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar";
 import SignIn from "./components/SignIn";
 import PasswordReset from "./components/PasswordReset";
 import AdminHome from "./components/AdminHome";
+import EmployeeDetails from "./components/EmployeeDetails";
 
 function App() {
 	return (
@@ -13,6 +14,7 @@ function App() {
 					<Route path="/" element={<><NavBar/> <SignIn/></>} />
           <Route path="/passwordreset" element={<><NavBar/> <PasswordReset/></>} />
           <Route path="/adminhome" element={<><NavBar/> <AdminHome/></>} />
+          <Route path="/employeedetails" element={<><NavBar/> <EmployeeDetails/></>} />
 				</Routes>
 			</BrowserRouter>
 		</div>
