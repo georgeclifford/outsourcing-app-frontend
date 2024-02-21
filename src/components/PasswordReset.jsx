1a43581b0ebb68e7b5857697926609401f6bdae6
georgeclifford <grgclffrd@gmail.com>
2024-02-21 05:40:54 +0530

    completed admin home page

--- src/components/PasswordReset.jsx
+++ src/components/PasswordReset.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import home_img from "../assets/images/home.jpeg";
 
 const PasswordReset = () => {
 	return (
-		<div style={{
-            height: "90vh",
-            backgroundImage:
-                "url('https://images.unsplash.com/photo-1637262448017-0fbbec87a898?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
-        }}>
+		<div
+			style={{
+				height: "70rem",
+				backgroundImage: "url(" + home_img + ")",
+				backgroundPosition: "1420px 1320px",
+			}}>
 			<div className="container-fluid p-5">
 				<div className="row justify-content-end">
 					<div className="card col col-12 col-sm-12 col-md-7 col-lg-6 col-xl-5 col-xxl-4 shadow-lg rounded p-5">
@@ -33,7 +35,7 @@ const PasswordReset = () => {
 								<label htmlFor="floatingPassword1">New Password</label>
 							</div>
 
-                            <div className="form-floating mb-4">
+							<div className="form-floating mb-4">
 								<input
 									type="password"
 									className="form-control shadow-sm"
@@ -44,9 +46,7 @@ const PasswordReset = () => {
 							</div>
 
 							<div className="d-grid pt-3">
-								<button
-									type="submit"
-									className="btn btn-fed shadow-sm">
+								<button type="submit" className="btn btn-fed shadow-sm">
 									Request Reset
 								</button>
 							</div>
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import home_img from "../assets/images/home.jpeg";
 
 const SignIn = () => {
 	return (
-		<div style={{
-            height: "90vh",
-            backgroundImage:
-                "url('https://images.unsplash.com/photo-1637262448017-0fbbec87a898?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
-        }}>
-			<div
-				className="container-fluid p-5"
-				>
+		<div
+			style={{
+				height: "70rem",
+				backgroundImage: "url(" + home_img + ")",
+				backgroundPosition: "1420px 1320px",
+			}}>
+			<div className="container-fluid p-5">
 				<div className="row justify-content-end">
 					<div className="card col col-12 col-sm-12 col-md-7 col-lg-6 col-xl-5 col-xxl-4 shadow-lg rounded p-5">
 						<h4 className="text-center">Sign In</h4>
